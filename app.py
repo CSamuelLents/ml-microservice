09e387cefb6e604bd7ac09ac4c8bb67c77ec2192
CSamuelLents <sam@samlents.com>
2021-04-11 14:21:05 -0400

    remove unnecessary f string

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 
