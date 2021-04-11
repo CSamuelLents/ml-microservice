0d31a3bb0186fe87f059679d5bac68f1f5fd4a45
CSamuelLents <sam@samlents.com>
2021-04-11 16:10:31 -0400

    increase verbosity of output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -7,11 +7,8 @@ dockerpath=csamuellents/ml-microservice
 kubectl create deployment ml-microservice --image="$dockerpath:latest"
 
 # Step 3:
-kubectl get pods
+kubectl get pods --output wide
 
 # Step 4:
 kubectl expose deployment/ml-microservice --type="NodePort" --port=80
-
-# Step 5:
-sleep 15
 kubectl port-forward deployment/ml-microservice 8000:80
